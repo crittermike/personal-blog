9166f97354573891d8c0eb0a9fdba3732ca0d3f1
Mike Crittenden <mikecrittenden@gmail.com>
2018-04-16 11:35:53 -0400

    Whoops, removed a vital plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,13 @@ module.exports = {
         {
             resolve: 'gatsby-source-filesystem',
             options: {
+                path: `${__dirname}/src/pages`,
+                name: 'pages',
+            },
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
                 path: `${__dirname}/src/img`,
                 name: 'images',
             },
