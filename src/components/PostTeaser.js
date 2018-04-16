00df98e7149d229cc1b393fd132af201ee4f7826
Mike Crittenden <mikecrittenden@gmail.com>
2018-04-16 12:01:30 -0400

    Change layout of blog listing

--- src/components/PostTeaser.js
+++ src/components/PostTeaser.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const PostTeaser = (props) => (
-    <div>
+    <div className="teaser">
         <h3><Link to={props.post.fields.slug}>{props.post.frontmatter.title}</Link></h3>
-        <p><small>{props.post.frontmatter.date} - {props.post.frontmatter.description}</small></p><br /><br />
+        <p><small>{props.post.frontmatter.date}</small></p>
     </div>
 )
 
