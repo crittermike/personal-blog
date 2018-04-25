fc52877604628c84a6207fc0f8d59835c895f31a
Mike Crittenden <mikecrittenden@gmail.com>
2018-04-25 09:40:51 -0400

    Fix footer spaces

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,7 @@ import React from 'react'
 
 const Footer = () => (
   <p className="footerlinks">
-    <a href="https://twitter.com/mcrittenden">Twitter</a> / 
-    <a href="https://www.linkedin.com/in/mikecrittenden/">LinkedIn</a> / 
-    <a href="https://github.com/mikecrittenden">GitHub</a> / 
-    <a href="https://drupal.org/u/mcrittenden">Drupal</a> / 
-    <a href="mailto:mikecrittenden@gmail.com">Email</a>
+    <a href="https://twitter.com/mcrittenden">Twitter</a> / <a href="https://www.linkedin.com/in/mikecrittenden/">LinkedIn</a> / <a href="https://github.com/mikecrittenden">GitHub</a> / <a href="https://drupal.org/u/mcrittenden">Drupal</a> / <a href="mailto:mikecrittenden@gmail.com">Email</a>
   </p>
 )
 
