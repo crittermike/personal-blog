938b8760833e095df9184955c3cc3bea21729d09
Mike Crittenden <mikecrittenden@gmail.com>
2018-05-17 10:28:40 -0400

    Remove Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,12 +39,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-netlify-cms`,
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-feed`,
       options: {
         feeds: [
