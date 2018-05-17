d291697a51e0cd9a9161eb99734c89d3f766302d
Mike Crittenden <mikecrittenden@gmail.com>
2018-05-17 10:17:16 -0400

    Add gatsby offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
         // icon: "src/images/icon.png", // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
